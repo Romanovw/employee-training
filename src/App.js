149da67595f942bd3ab275f578fbfbc080e315ae
Volodymyr Romanov <romanov.vw@gmail.com>
2023-07-22 20:45:14 +0200

    Start page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/main.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="links-container">
+        <a href="#" className="button1">KEBAB NA CIENKIM CIEĆCIE</a>
+        <a href="#" className="button2">TORTILLA</a>
+        <a href="#" className="button3">KEBAB W BULCE</a>
+        <a href="#" className="button4">Burgery</a>
+        <a href="#" className="button5">FRYTKI/SERVETS</a>
+        <a href="#" className="button6">FRYTKI Z POSYPKĄ</a>
+        <a href="#" className="button7">ZESTAW OBIADOWY</a>
+        <a href="#" className="button8">KEBAB BOX</a>
+        <a href="#" className="button9">SAŁATKI</a>
+        <a href="#" className="button10">FALAFEL MENU</a>
+        <a href="#" className="button11">Kanapki Panini</a>
+        <a href="#" className="button12">Nuggetsy</a>
+        <a href="#" className="button13">DODATKI</a>
+        <a href="#" className="button14">OPAKOWANIA</a>
+        <a href="#" className="button15">Napoje</a>
+        <a href="#" className="button16">Napoje-puszka</a>
+        <a href="#" className="button17">PIWO</a>
+        <a href="#" className="button18">Sosy</a>
+      </div>
     </div>
   );
 }
