c4f650168a2396ffc520c114bff6fa243ea9ded8
Volodymyr Romanov <romanov.vw@gmail.com>
2023-07-26 21:49:02 +0200

    Full second menu

--- src/components/Packaging/Packaging.js
+++ src/components/Packaging/Packaging.js
@@ -5,9 +5,9 @@ function Packaging() {
         <div className="links-container-pack">
         <a href="#" className="button1pack">RECYKLING REKLAMÓWKA</a>
         <a href="#" className="button2pack">KOSZT DOSTAWY</a>
-        <a href="#" className="button3pack">KOSZT DOSTAWU UBER/PUSZNE/GLOVO</a>
+        <a href="#" className="button3pack">KOSZT DOSTAWY UBER/ PYSZNE/ GLOVO</a>
         <a href="#" className="button4pack">OPAKOWANIE</a>
-        <a href="#" className="button5pack">USŁUGA GASTRONOMICZNA</a>
+        <a href="#" className="button5pack">USŁUGA Gastronomiczna</a>
         </div>
       </div>
     );
